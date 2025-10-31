9d0575246b310858c5f2bb30f61a90d412d085c6
GyaneshBamrara <112647673+GyaneshBamrara@users.noreply.github.com>
2025-10-31 14:10:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,106 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_vpc" "TerraF_vpc" {
-  id = "vpc-07557001c6a5eb1b9"
+# Generate a random suffix for unique bucket name
+resource "random_id" "rand" {
+  byte_length = 4
 }
 
-data "aws_subnet" "Terra_subnet" {
-  id = "subnet-03d4037033d0eb90f"
-}
-
-data "aws_instance" "Terraform_ins" {
-  instance_id = "i-09e0d352f1e8d4da1"
-}
-
-data "aws_s3_bucket" "terraformgb_s3_bucket" {
+# Create an S3 Bucket
+resource "aws_s3_bucket" "terraformgb_bucket" {
   bucket = "terraformgb-s3-bucket"
+  tags = {
+    Name        = "terraformgb-s3-bucket"
+    Environment = "Dev"
+  }
 }
 
-data "aws_key_pair" "k3" {
-  key_name = "k3"
+# Create a VPC
+resource "aws_vpc" "terra_vpc" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "TerraF_vpc"
+  }
+}
+
+# Create a public subnet inside that VPC
+resource "aws_subnet" "terra_subnet" {
+  vpc_id                  = aws_vpc.terra_vpc.id
+  cidr_block              = "10.0.0.0/28"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Terra_subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+  tags = {
+    Name = "Terra_IGW"
+  }
+}
+
+# Create a route table and attach to subnet
+resource "aws_route_table" "terra_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terra_igw.id
+  }
+  tags = {
+    Name = "Terra_RouteTable"
+  }
+}
+
+resource "aws_route_table_association" "terra_rta" {
+  subnet_id      = aws_subnet.terra_subnet.id
+  route_table_id = aws_route_table.terra_rt.id
+}
+
+# Create a security group
+resource "aws_security_group" "terra_sg" {
+  name        = "terra-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.terra_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terra_SecurityGroup"
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "terraform_ins" {
+  ami                         = "ami-01760eea5c574eb86" # Amazon Linux 2023
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.terra_subnet.id
+  vpc_security_group_ids      = [aws_security_group.terra_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "k3"
+
+  tags = {
+    Name = "Terraform_ins"
+  }
 }
