b014a50251f04e37a805a0b374017f0267ba4cf4
GyaneshBamrara <112647673+GyaneshBamrara@users.noreply.github.com>
2025-10-31 14:51:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_instance" "terraform_ins" {
   subnet_id                   = aws_subnet.terra_subnet.id
   vpc_security_group_ids      = [aws_security_group.terra_sg.id]
   associate_public_ip_address = true
-                     = "KeyTerra"
+  key_name = "KeyTerra"
 
   tags = {
     Name = "Terraform_ins"
